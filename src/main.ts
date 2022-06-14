a5455321b3b4b15f888dc98c8419d4c8f379c849
Hironao OTSUBO <motemen@gmail.com>
2022-06-14 22:40:54 +0900

    なんかでるようになった

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,61 @@
-import './style.css'
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { VRM } from "@pixiv/three-vrm";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import "./style.css";
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(10, 10, 10);
+scene.add(directionalLight);
+
+// camera.position.z = 2;
+camera.position.y = 1.2;
+camera.position.z = -0.8;
+camera.rotation.y = Math.PI * 1;
+
+const loader = new GLTFLoader();
+loader.load(
+  // URL of the VRM you want to load
+  "/models/AvatarSample_B.vrm",
+
+  // called when the resource is loaded
+  (gltf) => {
+    // generate a VRM instance from gltf
+    VRM.from(gltf).then((vrm) => {
+      // add the loaded vrm to the scene
+      scene.add(vrm.scene);
+
+      // deal with vrm features
+      console.log(vrm);
+    });
+  },
+
+  // called while loading is progressing
+  (progress) =>
+    console.log(
+      "Loading model...",
+      100.0 * (progress.loaded / progress.total),
+      "%"
+    ),
+
+  // called when loading has errors
+  (error) => console.error(error)
+);
