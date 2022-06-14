256d2b9d6f927fdfba8a16398d3b3c75fcf49b03
Hironao OTSUBO <motemen@gmail.com>
2022-06-14 22:55:32 +0900

    なんかうごかします

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,9 @@ const light = new THREE.DirectionalLight(0xffffff);
 light.position.set(1.0, 1.0, 1.0).normalize();
 scene.add(light);
 
+let currentVrm: VRM | undefined = undefined;
+let currentMixer: THREE.AnimationMixer | undefined = undefined;
+
 // gltf and vrm
 const loader = new GLTFLoader();
 loader.crossOrigin = "anonymous";
@@ -52,9 +55,13 @@ loader.load(
       console.log(vrm);
       scene.add(vrm.scene);
 
+      currentVrm = vrm;
+
       vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.Hips).rotation.y =
         Math.PI;
       vrm.springBoneManager!.reset();
+
+      prepareAnimation(vrm);
     });
   },
 
@@ -70,6 +77,34 @@ loader.load(
   (error) => console.error(error)
 );
 
+// animation
+function prepareAnimation(vrm: VRM) {
+  currentMixer = new THREE.AnimationMixer(vrm.scene);
+
+  const quatA = new THREE.Quaternion(0.0, 0.0, 0.0, 1.0);
+  const quatB = new THREE.Quaternion(0.0, 0.0, 0.0, 1.0);
+  quatB.setFromEuler(new THREE.Euler(0.0, 0.0, 0.25 * Math.PI));
+
+  const armTrack = new THREE.QuaternionKeyframeTrack(
+    vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm).name +
+      ".quaternion", // name
+    [0.0, 0.5, 1.0], // times
+    [...quatA.toArray(), ...quatB.toArray(), ...quatA.toArray()] // values
+  );
+
+  const blinkTrack = new THREE.NumberKeyframeTrack(
+    vrm.blendShapeProxy!.getBlendShapeTrackName(
+      VRMSchema.BlendShapePresetName.Blink
+    )!, // name
+    [0.0, 0.5, 1.0], // times
+    [0.0, 1.0, 0.0] // values
+  );
+
+  const clip = new THREE.AnimationClip("blink", 1.0, [armTrack, blinkTrack]);
+  const action = currentMixer.clipAction(clip);
+  action.play();
+}
+
 // helpers
 const gridHelper = new THREE.GridHelper(10, 10);
 scene.add(gridHelper);
@@ -77,8 +112,20 @@ scene.add(gridHelper);
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
+const clock = new THREE.Clock();
+
 function animate() {
   requestAnimationFrame(animate);
+
+  const deltaTime = clock.getDelta();
+
+  if (currentVrm) {
+    currentVrm.update(deltaTime);
+  }
+
+  if (currentMixer) {
+    currentMixer.update(deltaTime);
+  }
 
   renderer.render(scene, camera);
 }
