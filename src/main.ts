18724b20148e1ad7a1cd13ae92bd26a9620b11ee
Hironao OTSUBO <motemen@gmail.com>
2022-06-14 22:46:45 +0900

    three-vrm のサンプルがあった

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,24 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { VRM } from "@pixiv/three-vrm";
+import { VRM, VRMUtils, VRMSchema } from "@pixiv/three-vrm";
 
 import "./style.css";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
+// renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-function animate() {
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-}
-animate();
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(10, 10, 10);
-scene.add(directionalLight);
-
-// camera.position.z = 2;
-camera.position.y = 1.2;
-camera.position.z = -1;
-camera.rotation.y = Math.PI * 1;
+// camera
+const camera = new THREE.PerspectiveCamera(
+  30.0,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  20.0
+);
+camera.position.set(0.0, 1.0, 5.0);
 
 // camera controls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -38,20 +26,35 @@ controls.screenSpacePanning = true;
 controls.target.set(0.0, 1.0, 0.0);
 controls.update();
 
+// scene
+const scene = new THREE.Scene();
+
+// light
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.set(1.0, 1.0, 1.0).normalize();
+scene.add(light);
+
+// gltf and vrm
 const loader = new GLTFLoader();
+loader.crossOrigin = "anonymous";
 loader.load(
   // URL of the VRM you want to load
   "/models/AvatarSample_B.vrm",
 
   // called when the resource is loaded
   (gltf) => {
-    // generate a VRM instance from gltf
+    // calling these functions greatly improves the performance
+    VRMUtils.removeUnnecessaryVertices(gltf.scene);
+    VRMUtils.removeUnnecessaryJoints(gltf.scene);
+
+    // generate VRM instance from gltf
     VRM.from(gltf).then((vrm) => {
-      // add the loaded vrm to the scene
+      console.log(vrm);
       scene.add(vrm.scene);
 
-      // deal with vrm features
-      console.log(vrm);
+      vrm.humanoid!.getBoneNode(VRMSchema.HumanoidBoneName.Hips).rotation.y =
+        Math.PI;
+      vrm.springBoneManager!.reset();
     });
   },
 
@@ -66,3 +69,18 @@ loader.load(
   // called when loading has errors
   (error) => console.error(error)
 );
+
+// helpers
+const gridHelper = new THREE.GridHelper(10, 10);
+scene.add(gridHelper);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+animate();
